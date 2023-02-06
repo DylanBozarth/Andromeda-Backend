26c12e725abd49dc7fe1e4579c86f73c5c1b8ffb
Paul Mason <webdevpaulmason@gmail.com>
2023-02-06 13:47:07 -0800

    Add basic validation

--- src/buildings/dto/create-building.dto.ts
+++ src/buildings/dto/create-building.dto.ts
@@ -1,6 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateBuildingDto {
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly description: string;
+  @IsString()
   readonly imageSrc: string;
+  @IsNumber()
   readonly cost: number;
 }
