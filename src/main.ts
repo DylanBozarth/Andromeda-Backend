f0b3277c68b23d96de8cc15ae1e70c6531a634d2
Paul Mason <webdevpaulmason@gmail.com>
2023-02-20 03:52:52 -0800

    PORT

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
