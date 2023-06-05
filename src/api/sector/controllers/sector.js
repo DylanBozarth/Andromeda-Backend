618a431d7821438c7ef1bc3a8042e8eda0252de7
Dylan <dylanabozarth@gmail.com>
2023-06-05 11:44:57 -0700

    strapi working

--- src/api/sector/controllers/sector.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sector controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sector.sector');
--- src/api/sector/routes/sector.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sector router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sector.sector');
