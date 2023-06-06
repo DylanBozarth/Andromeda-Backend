0029c5263451cd682f7ac8df0ba523aa444847ab
Dylan <dylanabozarth@gmail.com>
2023-06-05 18:11:58 -0700

    update config

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', 'andromeda-backend-production.up.railway.app'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS", [`${process.env.API_KEY1}`, `${process.env.API_KEY2}`]),
