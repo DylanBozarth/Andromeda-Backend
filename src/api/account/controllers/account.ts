510826aaf808abfd6877bb4e763fb91fca4147a0
Dylan <dylanabozarth@gmail.com>
2023-09-03 10:57:49 -0700

    new vairables

--- src/api/account/controllers/account.ts
+++
@@ -1,7 +0,0 @@
-/**
- * account controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::account.account');
--- src/api/test/services/test.ts
+++
@@ -1,7 +0,0 @@
-/**
- * test service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::test.test');
