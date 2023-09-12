ab17173d4956bedf330a7cf15d443293ca0d168a
dylan <dylanabozarth@gmail.com>
2023-09-11 21:29:12 -0700

    update sample

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
+import { db } from './database/dbConnection';
+
 const server = Bun.serve({
-    port: 2999,
+    port: process.env.PORT,
     development: true,
     fetch(req) {
       const url = new URL(req.url);
       if (url.pathname === "/") return new Response(`Home page!`);
-      if (url.pathname === "/blog") return new Response("Blog!");
+      if (url.pathname === "/db") return new Response("database");
       return new Response(`404!`);
     },
   });
