4fdf3822855a364bbf2783186d2cce0d00551ccf
dylan <dylanabozarth@gmail.com>
2023-09-15 10:21:09 -0700

    eliyisa

--- database/dbConnection.ts
+++
@@ -1,9 +0,0 @@
-import { Database } from '@bun/database';
-
-export const db = new Database({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
