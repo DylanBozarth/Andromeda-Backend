5382d71aed6c5f25c9473aee115ab4273ed0219d
dylan <dylanabozarth@gmail.com>
2023-09-15 12:33:04 -0700

    routes and stuff

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import { Login } from "./login/login";
 import { Sectors } from "./sectors/sectors";
 
-const app = new Elysia().get("/", () => "Hello Elysia")
+const app = new Elysia().get("/", () => "DA BACKEND")
 .get('/sectors', () => Sectors)
 .get('/login', () => Login)
 .listen(2999);
---
+++ sectors/sectors.ts
@@ -0,0 +1,4 @@
+import { Elysia } from "elysia";
+import { cookie } from "@elysiajs/cookie";
+import { jwt } from "@elysiajs/jwt";
+export const Sectors = new Elysia() 
