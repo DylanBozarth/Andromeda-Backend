a64b20b2135078698a1fcd09231c730d958261b9
Dylan <dylanabozarth@gmail.com>
2023-09-19 21:37:57 -0700

    using prisma

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Login } from "./login/login";
 import { Sectors } from "./sectors/sectors";
 
 const app = new Elysia().get("/", () => "DA BACKEND")
-.get('/sectors', () => Sectors)
+.get('/sectora', ({query}) => `query:  ${query.q}`)
 .get('/login', () => Login)
 .listen(2999);
 
--- sectors/sectors.ts
+++ sectors/sectors.ts
@@ -1,4 +1,3 @@
 import { Elysia } from "elysia";
-import { cookie } from "@elysiajs/cookie";
-import { jwt } from "@elysiajs/jwt";
+
 export const Sectors = new Elysia() 
