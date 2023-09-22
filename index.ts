57ed68cbbfe63f4b7747d56091f86aa9765e8538
Dylan <dylanabozarth@gmail.com>
2023-09-22 16:12:32 -0700

    added notes

--- index.ts
+++ index.ts
@@ -6,11 +6,11 @@ await mongoose.connect(`${process.env.MONGOURL}`);
 
 const returnSector = await SectorModel;
 const app = new Elysia().get("/", () => "DA BACKEND")
-.get('/sectora', () => {return returnSector.find()})
-.get ('/test', () =>{ return'hello'})
-
-.listen(`${process.env.PORT}`); 
+  .get('/sectora', () => { return returnSector.find({}) })
+  .get('/test', () => { return 'hello' })
+  .listen(`${process.env.PORT}`);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+//db.sectora.find()
--- notes/addNewSector.md
+++ notes/addNewSector.md
@@ -1,4 +1,4 @@
 // Add new sector or any other document
 // replace the mongo url with the actual url 
 // do not run in the railway app 
-railway run mongoimport --authenticationDatabase=admin  ${MONGOURL} --collection Sectors --type json --file test.json
+railway run mongoimport --authenticationDatabase=admin  ${MONGOURL} --collection sectora --type json --file sectorA.json
