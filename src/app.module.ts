306f635cd5dcdf821899ae5f08a50b16b8a716d0
Dylan <dylanabozarth@gmail.com>
2023-09-23 14:15:59 -0700

    connected :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SectorsController } from './sectors/controller/sectors.controller';
 import { SectorsService } from './sectors/service/sectors.service';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot() // import the ENV file
 
 @Module({
-  // imports: [MongooseModule.forRoot(`${process.env.MONGOURL}`)],
+  imports: [MongooseModule.forRoot(`${process.env.MONGOURL}`, {dbName: 'Andromeda'})],
   controllers: [AppController, SectorsController],
   providers: [AppService, SectorsService],
 })
