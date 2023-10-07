80d1aea26a128f3131d7cac29744613eee34f617
Dylan <dylanabozarth@gmail.com>
2023-10-07 13:38:23 -0700

    solve this machine

--- src/sectors/sectors.controller.ts
+++ src/sectors/sectors.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { SectorInterface } from 'src/interfaces/sector-and-systems';
+import { SectorsService } from './sectors.service';
 
 @Controller('sectors')
 export class SectorsController {
-    @Get('/sectors')
+    constructor(private readonly sectorService: SectorsService) {}
+    @Get()
     async findAll(): Promise<any> {
-        return 'hell yeah';
+        return this.sectorService.findAll();
       }
 }
