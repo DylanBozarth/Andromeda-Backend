129852acab72b7b6ae9daf6178e1e2606b07a69a
Dylan <dylanabozarth@gmail.com>
2023-10-06 21:08:34 -0700

    prisma modles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SectorsController } from './sectors/sectors.controller';
 import { SectorsService } from './sectors/sectors.service';
 import { ConfigModule } from '@nestjs/config';
-import { SectorsModule } from './sectors/sectors.module';
+import { sectorModule } from './sectors/sectors.module';
 
 ConfigModule.forRoot() // import the ENV file
 
 @Module({
-  imports: [MongooseModule.forRoot(`${process.env.MONGOURL}`, {dbName: 'Andromeda'}), SectorsModule],
-  controllers: [AppController, SectorsController],
-  providers: [AppService, SectorsService],
+  imports: [MongooseModule.forRoot(`${process.env.MONGOURL}`, {dbName: 'Andromeda'}), sectorModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/sectors/sectors.service.ts
+++ src/sectors/sectors.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { SectorInterface } from 'src/interfaces/sector-and-systems';
-import { sectorSchema } from 'src/schema/sectorSchema';
 
 @Injectable()
-export class SectorService {
-  constructor(@InjectModel(sectorSchema.sectorName) private sectorModel: sectorSchema<SectorInterface>) {}
-
+export class SectorsService {
+  constructor(@InjectModel('Sector') private sectorModel: Model<SectorInterface>) {}
 
   async findAll(): Promise<SectorInterface[]> {
     return this.sectorModel.find().exec();
