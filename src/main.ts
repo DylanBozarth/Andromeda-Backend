ae18a0afcbe329b5ad26b5952816b5636c4483c9
Dylan <dylanabozarth@gmail.com>
2023-10-07 21:34:01 -0700

    debug

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+ConfigModule.forRoot() // import the ENV file
+const port = process.env.PORT || 3000;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, "0.0.0.0", function () {
+    console.log('MONGO URL IS: ', process.env.MONGOURL)
+  });;
+}
+bootstrap();
