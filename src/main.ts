c53c75aaf1c7d9219d6e38ba1f9a47deab64bf63
Dylan <dylanabozarth@gmail.com>
2023-10-12 21:31:06 -0700

    cors lite

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigModule } from '@nestjs/config';
 ConfigModule.forRoot() // import the ENV file
 const port = process.env.PORT || 3000;
-console.log('Mongo url is:', process.env.MONGO_URL)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors();
   await app.listen(port, "0.0.0.0", function () {
-    console.log("mongo url", process.env.MONGO_URL)
   });;
 }
 bootstrap();
