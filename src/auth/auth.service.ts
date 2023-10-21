a143e80e316b8e3c8b0d24bf61c01e59cfc7f877
Dylan <dylanabozarth@gmail.com>
2023-10-21 13:05:01 -0700

    fix typo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const payload = { sub: user.userId, username: user.username };
+    const payload = { sub: user.userId, username: user.userName };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
