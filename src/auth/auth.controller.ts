ecccafda515a34e46640cfb56801136abe7bfa62
Dylan <dylanabozarth@gmail.com>
2023-10-21 17:02:46 -0700

    JWT attempt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import { UsersService } from '../users/users.service';
 export class AuthController {
     constructor(private usersService: UsersService) {}
 
-    // @UseGuards(AuthGuard('local'))
+    @UseGuards(AuthGuard('jwt'))
     @Post('login')
     async login(@Request() req) {
         return req.user;
